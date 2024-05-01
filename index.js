fa968e55d4244eb02ac279aa74a69cfb18383280
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-05-01 11:53:01 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,15 @@
+let sozlar = ['non', 'asal', 'banan', 'olma', 'aziza', 'Gul', 'mashina', 'abdulloh'];
+let nbor = [];
+let nyoq = [];
+
+for(let i = 0; i < 8; i++){
+    if(sozlar[i].indexOf('n') === -1) {
+        nyoq.push(sozlar[i]);
+
+    } else {
+        nyoq.push(sozlar[i]);
+    }
+}
+
+console.log(nbor);
+console.log(nyoq);
